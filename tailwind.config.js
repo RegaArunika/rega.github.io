01f1b321009099fd18694f79490f05bdd34c7bca
Rega Arunika <regawp55@gmail.com>
2023-09-28 11:05:15 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html','about.html','portfolio.hmtl'],
+  theme: {
+    extend: {
+      colors: {
+        primary : '#f97316',
+        tulisan : '#1e40af'
+      }
+    },
+  },
+  plugins: [],
+}
+
